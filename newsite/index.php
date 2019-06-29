fd8f057e8c2358a24f5a31aa246f152a99c26aec
Catalina-R <stan.catalina@gmail.com>
2019-06-30 01:48:13 +0300

    create index.php

---
+++ newsite/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+</head>
+<body>
+<style>
+    body {
+            background-color: lightblue;
+          }
+          
+          h1 {
+            color: rgb(10, 10, 10);
+            text-align: center;
+          }
+          
+          p {
+            font-family: verdana;
+            font-size: 20px;
+          }
+</style>
+<!-- <h1><b>Bun venit pe site-ul meu!</b></h1>
+<h3>Voi incerca aici sa invat HTML si PHP.<br></h3>
+<p><b><i> ...Din nou :D.</i></b></p> -->
+
+    <h1>Home</h1>
+
+    <footer>
+        <p>MyWebsite &copy; 2019</p>
+    </footer>
+</body>
+</html> 
+
+<?php
+
+echo "Hello, World!";
